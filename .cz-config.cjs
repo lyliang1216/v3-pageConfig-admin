2c1172aa641ad96883ce61d9b6943cb94b54be7b
liuyinliang <1176440019@qq.com>
2024-07-24 14:06:33 +0800

    feat: 3
    undefined

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -17,6 +17,7 @@ module.exports = {
     type: '请选择提交类型:',
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
+    body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n)'
   },
@@ -24,5 +25,6 @@ module.exports = {
   allowCustomScopes: false,
   skipQuestions: ['scope', 'customScope', 'body', 'footer', 'breaking'],
   subjectLimit: 72,
-  askForBreakingChangeFirst: false
+  askForBreakingChangeFirst: false,
+  defaultBody: ''
 }

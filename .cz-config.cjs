01f10676d51dc908ca4dec48a428de7251b522ac
liuyinliang <1176440019@qq.com>
2024-07-24 14:01:20 +0800

    feat: 2
    undefined

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -17,13 +17,12 @@ module.exports = {
     type: '请选择提交类型:',
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n)'
   },
   allowBreakingChanges: ['feat', 'fix'],
   allowCustomScopes: false,
-  skipQuestions: ['scope', 'customScope', 'footer', 'breaking'],
+  skipQuestions: ['scope', 'customScope', 'body', 'footer', 'breaking'],
   subjectLimit: 72,
   askForBreakingChangeFirst: false
 }
